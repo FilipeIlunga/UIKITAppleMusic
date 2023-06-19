e36323f887b8e8ba55dabe04b8606aba6157c7a4
filipeilunga <filipeilunga@gmail.com>
2023-06-19 17:31:37 -0300

    Criando a xib

---
+++ UIKITAppleMusic/Storyboards/MusicRowTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MusicRowTableViewCell.swift
+//  UIKITAppleMusic
+//
+//  Created by Filipe Ilunga on 19/06/23.
+//
+
+import UIKit
+
+class MusicRowTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
