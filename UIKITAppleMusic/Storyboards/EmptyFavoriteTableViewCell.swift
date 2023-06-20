840e368bbac37abae3343f2d4be7eeac5575e086
filipeilunga <filipeilunga@gmail.com>
2023-06-20 10:00:30 -0300

    Ajustes na emptyTableViewCell

--- UIKITAppleMusic/Storyboards/EmptyFavoriteTableViewCell.swift
+++ UIKITAppleMusic/Storyboards/EmptyFavoriteTableViewCell.swift
@@ -9,15 +9,13 @@ import UIKit
 
 class EmptyFavoriteTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var emptyIcon: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
