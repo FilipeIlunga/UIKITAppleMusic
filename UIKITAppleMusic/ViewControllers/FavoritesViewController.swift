3cd43a5f4fd2610cfa270f3f340dd732ddbc993a
filipeilunga <filipeilunga@gmail.com>
2023-06-20 10:35:38 -0300

    Finalizando a favoriteViewController

--- UIKITAppleMusic/ViewControllers/FavoritesViewController.swift
+++ UIKITAppleMusic/ViewControllers/FavoritesViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class FavoritesViewController: UIViewController {
-       
+    
+    let favoriteMusics: [Music] = []
+    
     @IBOutlet weak var tableView: UITableView!
     
     let searchController = UISearchController()
@@ -32,23 +34,45 @@ extension FavoritesViewController: UISearchResultsUpdating {
 
 extension FavoritesViewController:  UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if favoriteMusics.count == 0 {
+            return 292.0
+        }
+        return 74
+    }
 }
 
 extension FavoritesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if favoriteMusics.isEmpty {
+            return 1
+        } else {
+            return favoriteMusics.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = UITableViewCell()
         
-        let cell = TableViewCellFactory.createCell(cellType: .musicRow, for: tableView, indexPath: indexPath) as! MusicRowTableViewCell
-                
-        cell.setCell(showFavorite: true)
+        if favoriteMusics.isEmpty {
+            let emptyCell = TableViewCellFactory.createCell(cellType: .emptyFavorite, for: tableView, indexPath: indexPath) as! EmptyFavoriteTableViewCell
+            
+            emptyCell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
+            cell = emptyCell
+            
+        } else {
+            let musicRowcell = TableViewCellFactory.createCell(cellType: .musicRow, for: tableView, indexPath: indexPath) as! MusicRowTableViewCell
+            
+            let music = favoriteMusics[indexPath.row]
+            
+            musicRowcell.tag = indexPath.row
+            
+            musicRowcell.setupCell(music: music, showFavorite: true)
+            cell = musicRowcell
+        }
         
-        cell.titleLabel.text = "Melhor eu ir/Ligando os Fatos/Sonho de Amor/Deixa eu te querer"
-      //  cell.posterImage.image = UIImage(systemName: "star")
-        cell.artistLabel.text = "ola"
+        
         return cell
     }
     
