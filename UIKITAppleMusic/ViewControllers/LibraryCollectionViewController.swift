f91ac19c5d1925aa82d895edaff250b6aaa3e57c
filipeilunga <filipeilunga@gmail.com>
2023-06-21 12:33:25 -0300

    Criando a LibraryCollectionViewController

---
+++ UIKITAppleMusic/ViewControllers/LibraryCollectionViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LibraryCollectionViewController.swift
+//  UIKITAppleMusic
+//
+//  Created by Filipe Ilunga on 21/06/23.
+//
+
+import UIKit
+
+class LibraryCollectionViewController: UIViewController {
+ 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var musicCategorires: [MusicCollectionCategory] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        navigationController?.navigationBar.prefersLargeTitles = false 
+    }
+    
+}
+
+extension LibraryCollectionViewController: UICollectionViewDelegate {
+    
+}
+
+extension LibraryCollectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return musicCategorires.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = CollectionViewCellFactory.createCell(cellType: .category, for: collectionView, indexPath: indexPath) as! CategoryCollectionViewCell
+        
+        let category = musicCategorires[indexPath.row]
+        
+        cell.categoryImage.image = UIImage(named: category.description)
+        cell.categoryName.text = category.description
+        
+        return cell
+    }
+    
+   
+}
